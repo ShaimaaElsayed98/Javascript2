531786809e4b6efb87d0e1d56f32769ce9348837
ShaimaaElsayed98 <53016795+ShaimaaElsayed98@users.noreply.github.com>
2019-10-18 16:21:11 +0300

    Update script.js

--- HW-Week6/script.js
+++ HW-Week6/script.js
@@ -32,3 +32,23 @@ function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
 
 
 threeFive(10, 15, threeCallback, fiveCallback);
+
+//Q3,4,5 THE LINKS DIDNT WORK, IT SAID PAGE NOT FOUND
+
+//Q7
+
+let x = 9; 
+function f1(val) { 
+    val = val+1; 
+    return val;
+}
+f1(x);
+console.log(x);  //it printed x as it just consoled log the value of x not the function
+
+let y = { x: 9 };
+function f2(val) {
+    val.x = val.x + 1;
+    return val;
+}
+f2(y);
+console.log(y); //it printed x=10, as y is an object and x is a key inside it the function saved a new value for x as a key.
